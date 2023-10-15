afb107f327c4d34f126be8bd1a90c614b6820499
Iker Blanco <iker@obsidiansolutions.es>
2023-10-15 23:22:33 +0200

    chore: add exercise 1

---
+++ src/exercise1/ThreadExercise.java
@@ -0,0 +1,33 @@
+package exercise1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
+
+public class ThreadExercise {
+
+    public static void main(String[] args) {
+        // Create 10 threads and start them
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new CustomRunnable(), "Thread-" + i);
+            thread.start();
+        }
+    }
+}
+
+class CustomRunnable implements Runnable {
+    // The creation time is final because it should not be changed after the thread is created
+    private final String creationTime;
+    // Add a logger to log the creation time of the thread
+    private static final Logger logger = Logger.getLogger(CustomRunnable.class.getName());
+
+    public CustomRunnable() {
+        creationTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
+    }
+
+    @Override
+    public void run() {
+        // Log the creation time of the thread
+        logger.info(Thread.currentThread().getName() + " created at " + creationTime);
+    }
+}
